0ff667b391fa4243f5490d76bf4f2e0aecee4b71
MythMega <45936113+MythMega@users.noreply.github.com>
2021-11-16 14:23:23 +0100

    First Files. Base code + UML

---
+++ www/base_page.php
@@ -0,0 +1,3 @@
+<?php include 'include/base.php'; ?>
+
+<h1>base page, start a new page from copying this one</h1>
---
+++ www/include/base.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <title>GF</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="bootstrap.bundle.js"></script>
+</head>
+<body>
+
+

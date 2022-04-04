199b99ded721734eb2db031f5158fcc5e5aa8999
MythMega <45936113+MythMega@users.noreply.github.com>
2022-04-04 16:27:46 +0200

    Add view, url and template for all list and details

--- www/Django/finder/views.py
+++ www/Django/finder/views.py
@@ -1,5 +1,4 @@
-from tkinter.messagebox import QUESTION
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from .models import *
 from django.template import loader
 
@@ -10,10 +9,78 @@ def index(request):
     data = {'last_submission_list': last_submission_list}
     return HttpResponse(template.render(data, request))
 
+
+
 def gameList(request):
     template = loader.get_template('finder/gamelist.html')
     data = {}
     return HttpResponse(template.render(data, request))
+
+def gameDetail(request, game_id):
+    try:
+        item = Game.objects.get(pk=game_id)
+    except Game.DoesNotExist:
+        raise Http404("This game does not exist")
+    return Game(request, 'finder/gamedetail.html', {'item':item})
+
+
+
+def editorList(request):
+    template = loader.get_template('finder/editorlist.html')
+    data = {}
+    return HttpResponse(template.render(data, request))
+
+def editorDetail(request, editor_id):
+    try:
+        item = Editor.objects.get(pk=editor_id)
+    except Editor.DoesNotExist:
+        raise Http404("This editor does not exist")
+    return Editor(request, 'finder/editordetail.html', {'item':item})
+
+
+
+
+def devList(request):
+    template = loader.get_template('finder/devlist.html')
+    data = {}
+    return HttpResponse(template.render(data, request))
+
+def devDetail(request, dev_id):
+    try:
+        item = Developer.objects.get(pk=dev_id)
+    except Developer.DoesNotExist:
+        raise Http404("This developer does not exist")
+    return Developer(request, 'finder/devdetail.html', {'item':item})
+
+
+
+def tagList(request):
+    template = loader.get_template('finder/taglist.html')
+    data = {}
+    return HttpResponse(template.render(data, request))
+
+def tagDetail(request, tag_id):
+    try:
+        item = Tag.objects.get(pk=tag_id)
+    except Tag.DoesNotExist:
+        raise Http404("This tag does not exist")
+    return Tag(request, 'finder/tagdetail.html', {'item':item})
+
+
+
+def platformList(request):
+    template = loader.get_template('finder/platformlist.html')
+    data = {}
+    return HttpResponse(template.render(data, request))
+
+def platformDetail(request, platform_id):
+    try:
+        item = Platform.objects.get(pk=platform_id)
+    except Platform.DoesNotExist:
+        raise Http404("This platform does not exist")
+    return Platform(request, 'finder/platformdetail.html', {'item':item})
+
+
 
 def pendingSubmissions(request):
     template = loader.get_template('finder/pendingSubmission.html')

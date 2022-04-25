5b436b0378e5ce737023216d4cedf61ec43a09f2
MythMega <45936113+MythMega@users.noreply.github.com>
2022-04-25 15:57:17 +0200

    Functions images

--- auto_commit_all.bat
+++ auto_commit_all.bat
@@ -1,4 +1,4 @@
 git pull https://github.com/MythMega/ADRAR_fil_rouge.git
 git add *
-git commit -m "Automatic Commit"
+git commit -m "Functions images"
 git push https://github.com/MythMega/ADRAR_fil_rouge.git
---
+++ www/Django/finder/functions.py
@@ -0,0 +1,23 @@
+"""
+La fonction pour obtenir des infos d'images
+"""
+def getImageInfos(monImage):
+    return {'size':monImage.size, 'height':monImage.height, 'width':monImage.width}
+
+"""
+La fonction pour obtenir le poid d'une image
+"""
+def getImageInfos(monImage):
+    return monImage.size
+
+"""
+La fonction pour obtenir la hauteur d'une image
+"""
+def getImageInfos(monImage):
+    return monImage.height
+
+"""
+La fonction pour obtenir la longueur d'une image
+"""
+def getImageInfos(monImage):
+    return monImage.width
